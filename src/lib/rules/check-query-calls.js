e32ba76fcf807c101de3e00f0b45e62defdf1883
Nicolas Froidure <nicolas.froidure@insertafter.com>
2015-09-02 10:33:26 +0200

    0.1.1

--- src/lib/rules/check-query-calls.js
+++ src/lib/rules/check-query-calls.js
@@ -16,9 +16,20 @@ function eMQCheckQueryCalls(context) {
           ' call first argument value to be an object.');
         return false;
       }
-      if(args[1] && 'ObjectExpression' !== args[1].type) {
+      if(args[1] && 'ObjectExpression' !== args[1].type &&
+        'FunctionExpression' !== args[1].type) {
         context.report(args[1], 'Expected ' + callSource +
-          ' call second argument value to be an object.');
+          ' call second argument value to be an object or a callback function.');
+        return false;
+      }
+      if(args[2] && 'FunctionExpression' !== args[2].type) {
+        context.report(args[2], 'Expected ' + callSource +
+          ' call third argument value to be a callback function.');
+        return false;
+      }
+      if(args[3]) {
+        context.report(node, 'Expected ' + callSource +
+          ' call to have maximum 3 arguments.');
         return false;
       }
       return true;
--- src/lib/rules/check-query-calls.mocha.js
+++ src/lib/rules/check-query-calls.mocha.js
@@ -12,6 +12,8 @@ ruleTester.run('check-query-calls', rule, {
     "mongoClient.db.collection('users').find({}, { limit: 10 });",
     "mongoClient.db.collection('users').findOne(gen(), {});",
     "mongoClient.db.collection('users').find(ref, {});",
+    "mongoClient.db.collection('users').find(ref, function() {});",
+    "mongoClient.db.collection('users').find(ref, {}, function() {});",
   ],
   invalid: [{
     code: "db.collection('users').find();",
@@ -26,7 +28,17 @@ ruleTester.run('check-query-calls', rule, {
   }, {
     code: "mongoClient.db.collection('users').find({}, 'test');",
     errors: [{
-      message: 'Expected mongoClient.db.collection(\'users\').find call second argument value to be an object.',
+      message: 'Expected mongoClient.db.collection(\'users\').find call second argument value to be an object or a callback function.',
+    }],
+  }, {
+    code: "mongoClient.db.collection('users').find({}, {}, 'test');",
+    errors: [{
+      message: 'Expected mongoClient.db.collection(\'users\').find call third argument value to be a callback function.',
+    }],
+  }, {
+    code: "mongoClient.db.collection('users').find({}, {}, function() {}, {});",
+    errors: [{
+      message: 'Expected mongoClient.db.collection(\'users\').find call to have maximum 3 arguments.',
     }],
   }],
 });
